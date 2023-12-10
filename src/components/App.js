e341d8d396e216721f01873c14e0740fde0cf47e
suraj0698 <annadatesuraj@gmail.com>
2023-12-10 19:21:30 +0530

    c2

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../styles/App.css';
 
 function App() {
     return (
-        <div id='root'>
+        <div id='main'>
             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
         </div>
     )
